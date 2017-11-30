ced8be7fe2997554f12322e6101a97eb540e8852
bennybop <bennybop74@yahoo.com>
2017-11-30 04:47:14 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -70,7 +70,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each {|key, value|
     puts "#{key.to_s.capitalize!}:"
     value.each {|key, value|
-    keyvalue = k.to_s.split("_").map(&:capitalize).join(" ")
+    keyvalue = key.to_s.split("_").map(&:capitalize).join(" ")
     puts " #{keyvalue}: #{value.join(", ")}"
   }
 }
